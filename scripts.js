f8270c98f4381889e6e0f5e375a051824f4425d9
Susan McKenzie <8696929+mathsusan@users.noreply.github.com>
2024-05-14 17:06:01 -0500

    header work

--- scripts.js
+++ scripts.js
@@ -1,8 +1,37 @@
 function toggleFunction() {
-  var x = document.getElementById("main-header");
-  if (x.classList.contains("menu")) {
-    x.classList.remove("menu");
+  var x = document.getElementById('main-header');
+  if (x.classList.contains('open')) {
+    x.classList.remove('open');
   } else {
-    x.classList.add("menu");
+    x.classList.add('open');
   }
 }
+
+const menulist = document.getElementsByClassName('menu-link');
+index = -1;
+
+document.addEventListener('keydown', (e) => {
+  //down key
+  if (e.key === 'ArrowDown') {
+    index++;
+    if (index === menulist.length) {
+      index = 0;
+    }
+    menulist[index].focus();
+  }
+
+  //upkey
+  else if (e.key === 'ArrowUp') {
+    index--;
+    if (index === -1) {
+      index = menulist.length - 1;
+    }
+    menulist[index].focus();
+  }
+
+  //Enter key
+  else if (e.key === 'Enter') {
+    console.log('Enter key was pressed ');
+    // menulist[index].click();
+  }
+});
